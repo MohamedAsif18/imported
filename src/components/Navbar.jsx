fb8350f53fb68859456790e750a86926edccb27f
MohamedAsif18 <158248974+MohamedAsif18@users.noreply.github.com>
2025-11-17 20:25:46 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const links = [
   { label: "Gallery", to: "/gallery" },
   { label: "Skills", to: "/skills" },
   { label: "Certificates", to: "/certificates" },
-  { label: "Blog", to: "/blog" },
+{/*  { label: "Blog", to: "/blog" }, */}
   { label: "Resume", to: "/resume" },
   { label: "About Me", to: "/about" },
   { label: "Contact", to: "/contact" },
